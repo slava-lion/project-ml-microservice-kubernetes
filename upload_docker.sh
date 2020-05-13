4a2c80c90b583345484b112a16807f8bfa7f0be6
slava-lion <slava.lion25@gmail.com>
2020-05-13 13:05:18 +0300

    added upload_docker script

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="slavalion/priceprediction"
+imagename="priceprediction"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag $imagename $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
