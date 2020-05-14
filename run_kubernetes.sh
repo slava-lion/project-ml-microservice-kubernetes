a412a8c7bf1cf22b0439be51321dfb7b7ec85bd1
slava-lion <slava.lion25@gmail.com>
2020-05-14 18:51:55 +0300

    added run_kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,22 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="slavalion/priceprediction"
+imagename="priceprediction"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+docker login
+kubectl run $imagename\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=$imagename
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $imagename 8000:80
